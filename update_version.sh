903e14f44a77c186efa1dff300b402297bfe149c
Vladimir <vlemp@vuksan.com>
2017-03-23 10:08:50 -0400

    Version bump

--- update_version.sh
+++ update_version.sh
@@ -2,6 +2,7 @@
 # we can debug easier
 VERSION=`cat VERSION`
 
-sed -i "s/resp.http.Fastly-Magento-VCL-Uploaded = \".*\"/resp.http.Fastly-Magento-VCL-Uploaded = \"$VERSION\"/g" ./app/code/community/Fastly/CDN/etc/default.vcl ./app/code/community/Fastly/CDN/etc/vcl_snippets/*.vcl
+sed -i "s/resp.http.Fastly-Magento-VCL-Uploaded = \".*\"/resp.http.Fastly-Magento-VCL-Uploaded = \"$VERSION\"/g" ./app/code/community/Fastly/CDN/etc/vcl_snippets/*.vcl
 sed -i "s/\"version\": \".*\"/\"version\": \"$VERSION\"/g" composer.json
 sed -i "s/\"Fastly-Module-Enabled\", \".*\"/\"Fastly-Module-Enabled\", \"$VERSION\"/g" app/code/community/Fastly/CDN/Helper/Cache.php
+sed -i "s|<version>.*</version>|<version>$VERSION</version>|g" app/code/community/Fastly/CDN/etc/config.xml
